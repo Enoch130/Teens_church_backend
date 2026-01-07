46c911d8ae7a3d917500b9ced2c71406f7db2eb5
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2026-01-07 01:06:00 +0000

    done with the first model

--- src/objects/user/models.py
+++ src/objects/user/models.py
@@ -3,6 +3,12 @@ from src.models.base import UUIDModel, TimestampedModel
 from sqlalchemy.dialects.postgresql import UUID as pgUUID
 from sqlalchemy.orm import Mapped, mapped_column
 
-class Homepage(UUIDModel, TimestampedModel):
-    title:Mapped[str] = mapped_column(String, nullable=False)
-    subtitle:Mapped[str] = mapped_column(String, nullable=False)
+class User(UUIDModel, TimestampedModel):
+    firstName:Mapped[str] = mapped_column(String)
+    middleName:Mapped[str] = mapped_column(String, nullable=True)
+    lastName:Mapped[str] = mapped_column(String)
+    email:Mapped[str] = mapped_column(String,unique=True, index = True)
+    phoneNumber:Mapped[str] = mapped_column(String,nullable=True)
+    password:Mapped[str] = mapped_column(String)
+    role:Mapped[str] = mapped_column(String)
+    
